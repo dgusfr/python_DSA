a8508ae5aaafc26090d20a6e23eaec1e0fc256b9
dgusfr <diegogustavo.fr@gmail.com>
2026-02-13 08:57:15 -0300

    feat: add Processador_de_Pedidos.py with order revenue calculation functionality

---
+++ Exercises/Processador_de_Pedidos.py
@@ -0,0 +1,27 @@
+"""
+Crie uma função em Python chamada calcular_faturamento_entregue que receba essa lista e retorne apenas o valor total (soma) de todos os pedidos que têm o status "entregue".
+"""
+
+pedidos = [
+    {"id": 1, "status": "pendente", "itens": [{"preco": 100, "qtd": 1}]},
+    {
+        "id": 2,
+        "status": "entregue",
+        "itens": [{"preco": 50, "qtd": 2}, {"preco": 200, "qtd": 1}],  # Total: 300
+    },
+    {"id": 3, "status": "cancelado", "itens": [{"preco": 500, "qtd": 1}]},
+    {"id": 4, "status": "entregue", "itens": [{"preco": 10, "qtd": 10}]},  # Total: 100
+]
+
+
+def calcular_faturamento_entregue(lista_pedidos):
+    valor_total = 0
+    for pedido in lista_pedidos:
+        if pedido["status"] == "entregue":
+            for item in pedido["itens"]:
+                valor_total = valor_total + (item["preco"] * item["qtd"])
+
+    print(f"O valor total dos pedidos entregues é {valor_total}")
+
+
+calcular_faturamento_entregue(pedidos)
