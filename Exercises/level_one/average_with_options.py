c6c68a32b98dca006e7519c4c8cc1def0f4d2735
dgusfr <diegogustavo.fr@gmail.com>
2026-02-19 23:22:40 -0300

    refactor: restructure input handling for weighted and harmonic averages

--- Exercises/level_one/average_with_options.py
+++ Exercises/level_one/average_with_options.py
@@ -1,24 +1,30 @@
-calculate = input("Enter either 'w' for weighted or 'h' for harmonic: ")
-if calculate == "w":
-    grade1 = float(input("Enter the first grade: "))
-    weight1 = float(input("Enter the weight for the first grade: "))
-    grade2 = float(input("Enter the second grade: "))
-    weight2 = float(input("Enter the weight for the second grade: "))
-    grade3 = float(input("Enter the third grade: "))
-    weight3 = float(input("Enter the weight for the third grade: "))
+exit = True
 
-    weighted_average = (grade1 * weight1 + grade2 * weight2 + grade3 * weight3) / (
-        weight1 + weight2 + weight3
-    )
-    print(f"The weighted average is: {weighted_average}")
+while exit:
+    calculate = input("Enter 'w' for weighted or 'h' for harmonic or 'e' for exit: ")
+    if calculate == "e":
+        exit = False
+        break
+    if calculate == "w":
+        grade1 = float(input("Enter the first grade: "))
+        weight1 = float(input("Enter the weight for the first grade: "))
+        grade2 = float(input("Enter the second grade: "))
+        weight2 = float(input("Enter the weight for the second grade: "))
+        grade3 = float(input("Enter the third grade: "))
+        weight3 = float(input("Enter the weight for the third grade: "))
 
-elif calculate == "h":
-    grade1 = float(input("Enter the first grade: "))
-    grade2 = float(input("Enter the second grade: "))
-    grade3 = float(input("Enter the third grade: "))
+        weighted_average = (grade1 * weight1 + grade2 * weight2 + grade3 * weight3) / (
+            weight1 + weight2 + weight3
+        )
+        print(f"The weighted average is: {weighted_average}")
 
-    harmonic_average = 3 / (1 / grade1 + 1 / grade2 + 1 / grade3)
-    print(f"The harmonic average is: {harmonic_average}")
+    elif calculate == "h":
+        grade1 = float(input("Enter the first grade: "))
+        grade2 = float(input("Enter the second grade: "))
+        grade3 = float(input("Enter the third grade: "))
 
-else:
-    print("Invalid option. Please enter 'w' for weighted or 'h' for harmonic.")
+        harmonic_average = 3 / (1 / grade1 + 1 / grade2 + 1 / grade3)
+        print(f"The harmonic average is: {harmonic_average}")
+
+    else:
+        print("Invalid option. Please enter 'w' for weighted or 'h' for harmonic.")
