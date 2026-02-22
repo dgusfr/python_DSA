0b9d4e5191513e17b1323c70f7062498c4914180
dgusfr <diegogustavo.fr@gmail.com>
2026-02-22 12:14:58 -0300

    refactor: change linear_search function to return None instead of -1 for not found

--- Search_algorithms/linear_search.py
+++ Search_algorithms/linear_search.py
@@ -6,7 +6,7 @@ def linear_search(arr, targetVal):
     for i in range(len(arr)):
         if arr[i] == targetVal:
             return i
-    return -1
+    return None
 
 
 result = linear_search(array, value)
