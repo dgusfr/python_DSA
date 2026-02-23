62819cdeba865f117819b7172036f5b671036a37
dgusfr <diegogustavo.fr@gmail.com>
2026-02-22 22:09:06 -0300

    feat: implement even or odd number checker in even_or_odd.py

--- Exercises/level_one/even_or_odd.py
+++ Exercises/level_one/even_or_odd.py
@@ -1,1 +1,9 @@
-""" """
+while True:
+    number = int(input("Digite um numero inteiro:"))
+    if number == -1:
+        break
+    else:
+        if number % 2 == 0:
+            print("O numero é par!")
+        else:
+            print("O numero é impar!")
