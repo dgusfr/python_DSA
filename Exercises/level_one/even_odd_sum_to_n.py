e891690d8f1d746b04bc5559eec459e7fa1da34d
dgusfr <diegogustavo.fr@gmail.com>
2026-02-24 18:11:53 -0300

    feat: implement even_odd_sum_to_n algorithm to calculate sums of even and odd numbers up to N

---
+++ Exercises/level_one/even_odd_sum_to_n.py
@@ -0,0 +1,19 @@
+"""
+Desenvolva um algoritmo que recebe um número N inteiro positivo e calcule a soma dos números inteiros pares e a soma dos inteiros ímpares de 1 até N.
+
+"""
+
+n = int(input("Digite um número inteiro: "))
+
+soma_pares = 0
+soma_impares = 0
+
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        soma_pares += i
+    else:
+        soma_impares += i
+
+
+print(f"A soma de numeros PARES de 1 até {n} é {soma_pares}")
+print(f"A soma de numeros IMPARES de 1 até {n} é {soma_impares}")
