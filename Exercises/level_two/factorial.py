8ce39e8663b5877b7f17b7fe8da39f8214609b54
dgusfr <diegogustavo.fr@gmail.com>
2026-02-25 17:39:19 -0300

    feat: add factorial.py to implement a function that calculates the factorial of a number

---
+++ Exercises/level_two/factorial.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+
+
+def fatorial(n):
+    fat = 1
+
+    for i in range(n + 1, 1, -1):
+        fat = fat * i
+        return fat
+
+
+print(fatorial(number))
