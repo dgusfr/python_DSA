fb31306f1c9340878925caeb6775c49137883b90
dgusfr <diegogustavo.fr@gmail.com>
2026-02-25 17:41:39 -0300

    fix: correct factorial calculation loop and return statement

--- Exercises/level_two/factorial.py
+++ Exercises/level_two/factorial.py
@@ -4,9 +4,10 @@ number = int(input("Enter a number: "))
 def fatorial(n):
     fat = 1
 
-    for i in range(n + 1, 1, -1):
+    for i in range(n, 1, -1):
         fat = fat * i
-        return fat
+
+    return fat
 
 
 print(fatorial(number))
