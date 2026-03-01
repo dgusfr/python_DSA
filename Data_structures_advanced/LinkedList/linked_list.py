f125d8cfd2333e05a95fdde3362b6033524037f9
dgusfr <diegogustavo.fr@gmail.com>
2026-03-01 11:58:43 -0300

    fix: rename parameters in __setitem__, index, insere_final_lista, insere_qualquer_posicao, and remove methods for consistency

--- Data_structures_advanced/LinkedList/linked_list.py
+++ Data_structures_advanced/LinkedList/linked_list.py
@@ -20,7 +20,7 @@ class LinkedList:
             return pointer.data
         raise IndexError("list index out of range")
 
-    def __setitem__(self, index, elemento):
+    def __setitem__(self, index, data):
         pointer = self.head
         for i in range(index):
             if pointer:
@@ -28,19 +28,19 @@ class LinkedList:
             else:
                 raise IndexError("list index out of range")
         if pointer:
-            pointer.data = elemento
+            pointer.data = data
         else:
             raise IndexError("list index out of range")
 
-    def index(self, elemento):
+    def index(self, data):
         pointer = self.head
         indice = 0
         while pointer:
-            if pointer.data == elemento:
+            if pointer.data == data:
                 return indice
             pointer = pointer.next
             indice = indice + 1
-        raise ValueError("{} is not in list".format(elemento))
+        raise ValueError("{} is not in list".format(data))
 
     def _getnode(self, index: int):
         if index < 0 or index >= self._size:
@@ -52,21 +52,21 @@ class LinkedList:
             pointer = pointer.next
         return pointer
 
-    def insere_final_lista(self, elemento):
+    def insere_final_lista(self, data):
         if self.head:
             pointer = self.head
             while pointer.next:
                 pointer = pointer.next
-            pointer.next = Node(elemento)
+            pointer.next = Node(data)
         else:
-            self.head = Node(elemento)
+            self.head = Node(data)
         self._size = self._size + 1
 
-    def insere_qualquer_posicao(self, index, elemento):
+    def insere_qualquer_posicao(self, index, data):
         if index < 0 or index > self._size:
             raise IndexError("list index out of range")
 
-        node = Node(elemento)
+        node = Node(data)
         if index == 0:
             node.next = self.head
             self.head = node
@@ -78,10 +78,10 @@ class LinkedList:
             anterior.next = node
         self._size = self._size + 1
 
-    def remove(self, elem):
+    def remove(self, data):
         if self.head == None:
-            raise ValueError("{} is not in list".format(elem))
-        elif self.head.data == elem:
+            raise ValueError("{} is not in list".format(data))
+        elif self.head.data == data:
             self.head = self.head.next
             self._size = self._size - 1
             return True
@@ -89,14 +89,14 @@ class LinkedList:
             ancestor = self.head
             pointer = self.head.next
             while pointer:
-                if pointer.data == elem:
+                if pointer.data == data:
                     ancestor.next = pointer.next
                     pointer.next = None
                     self._size = self._size - 1
                     return True
                 ancestor = pointer
                 pointer = pointer.next
-        raise ValueError("{} is not in list".format(elem))
+        raise ValueError("{} is not in list".format(data))
 
     def __repr__(self):
         r = ""
